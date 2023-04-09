d5f23f704fe6791e06eb1bc46f21b87e93ec0d5a
zaky06 <orochimaruimba@gmail.com>
2023-04-09 22:06:23 +0300

    jest tests

--- __test__/e2e/course.api.test.ts
+++ __test__/e2e/course.api.test.ts
@@ -31,21 +31,103 @@ describe('/course', () => {
             .expect(200, [])
     })
 
+    let createdCourse: any = null
+
     it('should create course with correct input data', async() => {
         const createResponce = await request(app)
             .post('/courses')
             .send({title: 'broNig'})
             .expect(201)
 
-        const createCourse = createResponce.body;
+        createdCourse = createResponce.body;
 
-        expect(createCourse).toEqual({
+        expect(createdCourse).toEqual({
             id: expect.any(Number),
             title: 'broNig'
         })
 
         await request(app)
             .get('/courses')
-            .expect(200, [createCourse])
+            .expect(200, [createdCourse])
     })
+
+    let createdCourse2: any = null;
+
+    it('create one more course', async() => {
+        const createResponce = await request(app)
+            .post('/courses')
+            .send({title: 'broNig2'})
+            .expect(201)
+
+        createdCourse2 = createResponce.body;
+
+        expect(createdCourse2).toEqual({
+            id: expect.any(Number),
+            title: 'broNig2'
+        })
+
+        await request(app)
+            .get('/courses')
+            .expect(200, [createdCourse, createdCourse2])
+    })
+
+    it('shouldnt update course with incorrect input data', async() => {
+        await request(app)
+            .put('/courses/' + createdCourse.id)
+            .send({title: ''})
+            .expect(400)
+
+        await request(app)
+            .get('/courses/' + createdCourse.id)
+            .expect(200, createdCourse)
+    })
+
+    it('shouldnt update course that not exist', async() => {
+        await request(app)
+            .put('/courses/' + -2)
+            .send({title: 'good title'})
+            .expect(404)
+    })
+
+    it('should update course with correct input data', async() => {
+        await request(app)
+            .put('/courses/' + createdCourse.id)
+            .send({title: 'good new title'})
+            .expect(204)
+
+        await request(app)
+            .get('/courses/' + createdCourse.id)
+            .expect(200, {...createdCourse,
+                 title: 'good new title'})
+
+        await request(app)
+            .get('/courses/' + createdCourse2.id)
+            .expect(200, createdCourse2)
+    })
+
+    it('should delete both courses', async() => {
+        await request(app)
+            .delete('/courses/' + createdCourse.id)
+            .expect(204)
+
+        await request(app)
+            .get('/courses/' + createdCourse.id)
+            .expect(404)
+
+        await request(app)
+            .delete('/courses/' + createdCourse2.id)
+            .expect(204)
+
+        await request(app)
+            .get('/courses/' + createdCourse2.id)
+            .expect(404)
+
+        await request(app)
+            .get('/courses')
+            .expect(200, [])
+    })
+
+    
+
+
 })
